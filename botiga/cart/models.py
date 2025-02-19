6c808483d744838296308f3f90f7f5fa17b9b554
getaudio2 <paugermaisar@hotmail.es>
2025-02-19 19:40:56 +0100

    Creat el model Cart

--- botiga/cart/models.py
+++ botiga/cart/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.CharField(max_length=30)
+
+class Cart(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(db_comment="Date time creation")
