1c83f12ccad18c84f480302e6ab0379121af175d
alexrss85 <2023_alex.rosendo@iticbcn.cat>
2025-02-19 19:33:23 +0100

    model_product

--- botiga/catalog/models.py
+++ botiga/catalog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    nom = models.EmailField(unique=True, max_length=50)  
+    descripcio = models.CharField(max_length=20) 
+    preu = models.CharField(max_length=20)  
+    stock = models.CharField(max_length=15)
+    categoria = models.CharField(max_length=15)
+    imatge = models.CharField(max_length=15)
