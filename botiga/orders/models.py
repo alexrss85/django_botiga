66793571ba08b95fdeb81b7c4d10e81a9cdb9d55
getaudio2 <paugermaisar@hotmail.es>
2025-02-24 17:20:39 +0100

    Creat model Order

--- botiga/orders/models.py
+++ botiga/orders/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Order(models.Model):
+    status_choice = [("pendent","Pendent"), ("completat","Completat"), ("Cancel·lat","Cancel·lat")]
+
+    user_id = models.ForeignKey('cart.User', on_delete=models.CASCADE)
+    total_price = models.IntegerField()
+    status = models.CharField(max_length=20, choices=status_choice, default="pending")
+    created_at = models.DateTimeField(auto_now_add=True)
